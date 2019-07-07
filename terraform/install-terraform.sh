9100e61997607e2c9eb3093934e15f07499fa839
naimabdrahman <naim.abdrahman@gmail.com>
2019-07-07 20:53:01 +0800

    Sun Jul  7 20:52:29 +08 2019

---
+++ terraform/install-terraform.sh
@@ -0,0 +1,15 @@
+## install terraform on centos 7
+## ensure the downloaded terraform package is up-to-date  
+
+yum -y update
+yum -y  install wget unzip
+
+cd; mkdir temp
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip -P /root/temp
+
+cd /root/temp
+unzip ./terraform_0.11.13_linux_amd64.zip -d /usr/local/bin/
+
+cd
+
+
