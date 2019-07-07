06b7d0a3fd1f47fca05b48b315b7d90e6c50e962
naimabdrahman <naim.abdrahman@gmail.com>
2019-07-07 22:07:19 +0800

    Update install-terraform.sh

--- terraform/install-terraform.sh
+++ terraform/install-terraform.sh
@@ -1,13 +1,14 @@
 ## install terraform on centos 7
 ## ensure the downloaded terraform package is up-to-date  
+## tested version with linode: https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_amd64.zip
 
 yum -y update
 yum -y  install wget unzip
 
 cd; mkdir /root/temp
-wget https://releases.hashicorp.com/terraform/0.12.3/terraform_0.12.3_linux_amd64.zip -P /root/temp
+wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_amd64.zip -P /root/temp
 
 cd /root/temp
-unzip ./terraform_0.12.3_linux_amd64.zip -d /usr/local/bin/
+unzip ./terraform_0.11.9_linux_amd64.zip -d /usr/local/bin/
 
 cd
