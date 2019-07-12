9fb307e14943ae38f634942f6d776825c833478b
naimabdrahman <naim.abdrahman@gmail.com>
2019-07-12 11:39:22 +0800

    Update linode-terraform-node.tf

--- terraform/linode-terraform-node.tf
+++ terraform/linode-terraform-node.tf
@@ -11,6 +11,7 @@ resource "linode_instance" "terraform-node" {
         group = "Terraform"
         region = "ap-south"
         type = "g6-nanode-1"
+        private_ip = "true"
         authorized_keys = [ "YOUR_PUBLIC_SSH_KEY" ]
         root_pass = "YOUR_ROOT_PASSWORD"
 }
